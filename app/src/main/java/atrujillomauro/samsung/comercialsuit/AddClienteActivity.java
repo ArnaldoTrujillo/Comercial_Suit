dfe99cb9c8a0bf5f8e9aa3e40228297f40f113b4
Arnaldo Trujillo <atrujillo92work@gmail.com>
2014-07-10 13:12:10 +0200

    añadida ActionBar personalizada roja

--- app/src/main/java/atrujillomauro/samsung/comercialsuit/AddClienteActivity.java
+++ app/src/main/java/atrujillomauro/samsung/comercialsuit/AddClienteActivity.java
@@ -1,25 +1,30 @@
 package atrujillomauro.samsung.comercialsuit;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class AddClienteActivity extends Activity {
 
     Spinner comisionSpinner;
+    EditText mNombreET, mApellidosET, mDireccionET, mCodigoPostalET, mTelefonoET;
     private String fecha;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_layout);
+        inicializarVistas();
         comisionSpinner = (Spinner) findViewById(R.id.comisionSpinner);
         ArrayAdapter<CharSequence> comisionAdapter = ArrayAdapter.createFromResource(this, R.array.comisiones_array, android.R.layout.simple_spinner_dropdown_item);
         comisionSpinner.setAdapter(comisionAdapter);
@@ -28,6 +33,14 @@ public class AddClienteActivity extends Activity {
         Calendar calendar = Calendar.getInstance();
         fecha = calendar.get(Calendar.YEAR) + "/" + calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.DAY_OF_MONTH);
 
+    }
+
+    private void inicializarVistas() {
+        mNombreET = (EditText) findViewById(R.id.nombreET);
+        mApellidosET = (EditText) findViewById(R.id.apellidoET);
+        mDireccionET = (EditText) findViewById(R.id.direccionET);
+        mCodigoPostalET = (EditText) findViewById(R.id.cpET);
+        mTelefonoET = (EditText) findViewById(R.id.telefonoET);
     }
 
 
@@ -57,5 +70,37 @@ public class AddClienteActivity extends Activity {
 
     protected void setFecha(String fecha) {
         this.fecha = fecha;
+    }
+
+    public void agregarClientes(View view) {
+        if (todoEstaLleno()) {
+            Toast.makeText(this, "Debe rellenar todos los campos", Toast.LENGTH_LONG).show();
+        } else {
+            insertarEnDb();
+            finish();
+        }
+    }
+
+    private void insertarEnDb() {
+        DBAdapter.DBHelper dbHelper = new DBAdapter(this).getDbHelper();
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        ContentValues valuesToInsert = new ContentValues();
+
+        valuesToInsert.put(DBAdapter.Columns.NOMBRE_COL, mNombreET.getText().toString());
+        valuesToInsert.put(DBAdapter.Columns.APELLIDOS_COL, mApellidosET.getText().toString());
+        String spinnerValue = comisionSpinner.getSelectedItem().toString();
+        int spinnerIntValue = Integer.parseInt(spinnerValue.substring(0, spinnerValue.indexOf("€")));
+        valuesToInsert.put(DBAdapter.Columns.COMISION_COL, spinnerIntValue);
+        valuesToInsert.put(DBAdapter.Columns.CP_COL, Integer.parseInt(mCodigoPostalET.getText().toString()));
+        valuesToInsert.put(DBAdapter.Columns.FECHA_COL, fecha);
+        valuesToInsert.put(DBAdapter.Columns.DIRECCION_COL, mDireccionET.getText().toString());
+        valuesToInsert.put(DBAdapter.Columns.TLF_COL, mTelefonoET.getText().toString());
+
+        database.insert(DBAdapter.TB_REGISTRO, null, valuesToInsert);
+        database.close();
+    }
+
+    private boolean todoEstaLleno() {
+        return !(mNombreET.length() != 0 && mApellidosET.length() != 0 && mDireccionET.length() != 0 && mCodigoPostalET.length() != 0 && mTelefonoET.length() != 0);
     }
 }
--- app/src/main/java/atrujillomauro/samsung/comercialsuit/DBAdapter.java
+++ app/src/main/java/atrujillomauro/samsung/comercialsuit/DBAdapter.java
@@ -7,24 +7,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 public class DBAdapter {
-    private final static int DB_VERSION = 1;
-    private final static String DB_NAME = "registroDB";
-    private final static String TB_REGISTRO = "clientesTable";
+    public final static String DB_NAME = "registroDB";
+    public final static String TB_REGISTRO = "clientesTable";
     private final static String CR_TABLE = "CREATE TABLE "
             + TB_REGISTRO + " (" + Columns.ID
             + " INTEGER PRIMARY KEY AUTOINCREMENT, " + Columns.NOMBRE_COL
             + " TEXT, " + Columns.APELLIDOS_COL + " TEXT NOT NULL, " + Columns.DIRECCION_COL
             + " TEXT NOT NULL, " + Columns.CP_COL + " INTEGER NOT NULL, " + Columns.COMISION_COL
-            + " INTENGER NOT NULL DEFAULT '0', " + Columns.TLF_COL + " INTEGER NOT NULL, "
+            + " INTENGER NOT NULL DEFAULT '0', " + Columns.TLF_COL + " TEXT, "
             + Columns.FECHA_COL + " DATE DEFAULT CURRENT_TIMESTAMP)";
-
+    private final static int DB_VERSION = 1;
     private final static String[] COLUMNS = {Columns.ID, Columns.NOMBRE_COL, Columns.APELLIDOS_COL, Columns.DIRECCION_COL,
             Columns.CP_COL, Columns.COMISION_COL, Columns.TLF_COL, Columns.FECHA_COL};
 
     private DBHelper dbHelper;
 
     public DBAdapter(Context context) {
-
         dbHelper = new DBHelper(context);
     }
 
